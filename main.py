aa7fa640524e64b240b099846145b14583c1b96b
Faezragore <30468128+Faezragore@users.noreply.github.com>
2022-09-02 23:00:24 +0300

    Update main.py
    correction of recommendations.

--- main.py
+++ main.py
@@ -10,9 +10,11 @@ from logging.handlers import RotatingFileHandler
 from logging import StreamHandler
 import asyncio
 
+
 logger = logging.getLogger('Бот логер')
 
-class MyLogsHandler(logging.Handler):
+
+class LogsHandler(logging.Handler):
     def __init__(self, tg_bot, tg_chat_id):
         super().__init__()
         self.tg_bot = tg_bot
@@ -22,44 +24,51 @@ class MyLogsHandler(logging.Handler):
         log_entry = self.format(record)
         self.tg_bot.send_message(text=log_entry, chat_id=self.tg_chat_id)
 
-def send_a_message_to_telegram_bot(bot, chat_id, message):
-    print(message)
-    bot.send_message(chat_id=chat_id, text=message)
 
 def main():
+    load_dotenv()
     telegram_token_bot = os.environ["TELEGRAM_TOKEN_BOT"]
     bot = telegram.Bot(token=telegram_token_bot)
     chat_id = os.environ["CHAT_ID"]
     devman_token = os.environ["DEVMAN_TOKEN"]
 
-    formatter = logging.Formatter("%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s")
+    formatter = logging.Formatter("%(filename)s[LINE:\
+                %(lineno)d]# %(levelname)-8s [%(asctime)s]\
+                %(message)s")
     console_handler = logging.StreamHandler()
     console_handler.setFormatter(formatter)
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.INFO)
-    logger.addHandler(MyLogsHandler(bot, chat_id))
+    logger.addHandler(LogsHandler(bot, chat_id))
     logger.info("Я новый логер!")
+    logger.info("Бот запущен")
 
     headers = {
         'Authorization': 'Token %s' % (devman_token)
     }
-    payload = {"timestamp":""}
+    payload = {"timestamp": ""}
     url = 'https://dvmn.org/api/long_polling/'
-    
+
     while True:
         try:
-            logger.info("Бот запущен")
-            response = requests.get(url, headers=headers, verify=True, params=payload, timeout=100)
+            response = requests.get(
+                url, headers=headers,
+                verify=True, params=payload, timeout=100
+            )
             response.raise_for_status()
-            response_from_server = response.json()
-            
-            if "timeout" in response_from_server["status"]:
-                payload["timestamp"] = response_from_server["timestamp_to_request"]
+            response_api_devman = response.json()
+
+            if "timeout" in response_api_devman["status"]:
+                payload["timestamp"] = \
+                    response_api_devman["timestamp_to_request"]
             else:
-                new_response_from_server = response_from_server["new_attempts"][0]
-                message = "У вас проверили работу:урок %s. Отправляем уведомления о проверке работ" % (new_response_from_server["lesson_title"])
-                send_a_message_to_telegram_bot(bot, chat_id, message)
-                payload["timestamp"] = new_response_from_server["timestamp"]
+                new_response_api_devman = \
+                    response_api_devman["new_attempts"][0]
+                message = "У вас проверили работу:урок %s.\
+                           Отправляем уведомления о проверке работ" \
+                          % (new_response_api_devman["lesson_title"])
+                bot.send_message(chat_id=chat_id, text=message)
+                payload["timestamp"] = new_response_api_devman["timestamp"]
                 continue
 
         except requests.exceptions.HTTPError as error:
@@ -74,6 +83,6 @@ def main():
         except Exception as error:
             logger.exception("Бот упал с ошибкой: {error}")
 
+
 if __name__ == '__main__':
-    load_dotenv()
     main()
